d48f3088d281241d79efc179ccded6d4841e816a
Kamil Chęciński <145483945+kejmil4@users.noreply.github.com>
2023-10-19 17:45:33 +0200

    Add files via upload

---
+++ Zad_13_10/index.php
@@ -0,0 +1,25 @@
+<!doctype html>
+<html lang="pl">
+<head>
+    <meta charset="UTF-8">
+    <title>Index</title>
+    <link rel="stylesheet" href="styl.css">
+</head>
+<body>
+<?php
+    include './partials/function.php';
+    echo podaj_naglowek("Strona główna", 1)
+ ?>
+
+
+    <p>Paragraf 1</p>
+<?php
+    require './partials/nav.php';
+    include './partials/footer.php';
+?>
+
+</body>
+</html>
+
+
+
